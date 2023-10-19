ef174ef1c186aa74c4d396d1c72d075a0afedf42
hyenaManager <zodiacjelly9@gmail.com>
2023-10-19 12:49:11 +0630

    modify:not found error

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -5,12 +5,25 @@ export default function NotFound() {
   return (
     <main className="fixed z-50 top-0 left-0 bg-white w-full h-full flex justify-center items-center">
       <section className=" flex flex-col items-center">
+        <Image
+          src={"/myconBlack.png"}
+          width={100}
+          height={100}
+          alt="notFoundError"
+        />
         <h2 className="text-4xl text-red-500">Not Found</h2>
-        <p className=" text-black">Could not find requested resource</p>
+        <p className=" text-black">Could not find requested resource </p>
+        <p className=" text-black">Navigate back to the following link </p>
         <div className="flex justify-between text-fuchsia-700 text-2xl items-center p-2">
-          <Link href="/">Home</Link>
-          <Link href="/clips">Clip</Link>
-          <Link href="/profile">Profile</Link>
+          <Link href="/" className="mr-3">
+            Home
+          </Link>
+          <Link href="/clips" className="mr-3">
+            Clip
+          </Link>
+          <Link href="/profile" className="mr-3">
+            Profile
+          </Link>
         </div>
       </section>
     </main>
