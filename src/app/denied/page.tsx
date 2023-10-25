fa92bfb0955c5bae2ef459a4b7a582fd4d1eda25
hyenaManager <zodiacjelly9@gmail.com>
2023-10-25 22:46:57 +0630

    merge with adminMOde

--- src/app/denied/page.tsx
+++ src/app/denied/page.tsx
@@ -1,7 +1,5 @@
+import React from "react";
+
 export default function Denied() {
-  return (
-    <div className=" pt-14 text-red-600 text-4xl text-center">
-      You are not allowed to access the page
-    </div>
-  );
+  return <div className=" w-full text-red-600 text-4xl">Access Denied</div>;
 }
