088c3ac7ce75e87ce1c52ea2803430bb5025a830
hyenaManager <zodiacjelly9@gmail.com>
2024-01-15 22:47:48 +0630

    change url for production

--- src/app/gettingStart/contact/page.tsx
+++ src/app/gettingStart/contact/page.tsx
@@ -17,13 +17,16 @@ export default function Contact() {
   const [isSubmiting, setIsSubmiting] = useState(false);
   const pageId = useCreatingPage((state) => state.createdPageId);
   async function createContact() {
-    const response = await axios.post("http://localhost:3000/api/contact", {
-      facebook: facebookLink,
-      twitter: twitterLink,
-      whatsapp: whatsappLink,
-      telegram: telegramLink,
-      pageId: pageId,
-    });
+    const response = await axios.post(
+      "https://yokeplay.vercel.app/api/contact",
+      {
+        facebook: facebookLink,
+        twitter: twitterLink,
+        whatsapp: whatsappLink,
+        telegram: telegramLink,
+        pageId: pageId,
+      }
+    );
     if (response.status === 200) {
       toast.success("working.....");
     } else {
